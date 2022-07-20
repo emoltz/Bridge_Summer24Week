cb07272c0decef94655d22adc7a4a13641218f8c
emoltz <es5541@nyu.edu>
2022-07-20 16:49:53 -0400

    "magic" formula

--- Ethan OH/July20.cpp
+++ Ethan OH/July20.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 int main(){
     double q = 5.0 / 9.0;
-
+    //FLOAT uses 32 bits
+    //DOUBLE uses 64 bits
     cout << q;
+
+    //"magic" formula for setting decimal precision
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    //the number in the parens below can set the precision of the decimal points.
+    cout.precision(2);
 
     return 0;
 }
