2ce28d3097b5ebc1c4d53fb78ee8454b7d1d137f
emoltz <es5541@nyu.edu>
2022-07-27 15:50:58 -0400

    july 27

--- Ethan OH/July27.cpp
+++ Ethan OH/July27.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+//for loops
+void forLoops(){
+    for (int i = 100; i > 10; i-=3) {
+        cout << i << " ";
+    }
 
+    cout << endl;
+    int i = 100;
+    while(i > 10){
+        cout << i << " ";
+        i -= 3;
+    }
+
+    int arr[5] = {1, 2, 3, 4, 5}; //static array
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] % 2 == 0){
+            cout << arr[i];
+        }
+        //break out of loop if we find a 2
+        if(arr[i] == 2){
+            break;
+        }
+    }
+
+    cout << endl;
+
+    int j = 0;
+    do{
+        j++;
+    }
+    while(j < 10);
+
+}
+
+void switchStatement(){
+    string s = "Mo";
+    string t = "Tu";
+
+    if (s == t){
+        cout << "true";
+    }
+    else{
+        cout << "false";
+    }
+}
+
+
+int main(){
+    switchStatement();
     return 0;
 }
