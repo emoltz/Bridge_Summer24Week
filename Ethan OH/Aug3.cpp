7d7bcab7c3c51f9bac2f88c74c79304fc6be8805
emoltz <es5541@nyu.edu>
2022-08-03 16:13:25 -0400

    shapes

---
+++ Ethan OH/Aug3.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+using namespace std;
+
+//GOAL: print a triangle with letters
+/*
+ *        a
+ *      b  b
+ *     c c  c
+ *     ...etc
+ */
+
+void makeTriangle(int n) {
+
+    char star = '*';
+    char space = ' ';
+
+    int spacesBegin = n - 1; //4
+    int starsBegin = 1;
+
+    for (int i = 0; i < n; i++) { //rows!
+        //spaces
+        for (int j = 0; j < spacesBegin; j++) {
+            cout << space;
+        }
+
+        //stars
+        for (int j = 0; j < starsBegin; j++) {
+            cout << star;
+        }
+
+        cout << endl;
+        spacesBegin--;
+        starsBegin += 2;
+    }
+
+}
+
+void makeLetterTriangle(int n) {
+    char letter = 'a';
+    char space = ' ';
+
+    int spacesBegin = n - 1; //4
+    int starsBegin = 1;
+
+    for (int i = 0; i < n; i++) { //rows!
+        //spaces
+        for (int j = 0; j < spacesBegin; j++) {
+            cout << space;
+        }
+
+        //stars
+        for (int j = 0; j < starsBegin; j++) {
+            cout << letter;
+            letter++;
+        }
+
+        cout << endl;
+        spacesBegin--;
+        starsBegin += 2;
+        letter = 'a'; //reset back to a
+    }
+}
+
+
+void doubleTriangle(int n) {
+    char star = '*';
+    char space = ' ';
+    int starLimit = 1;
+    int spaceLimit = 2 * n - 2; //8
+
+    for (int i = 0; i < (2 * n - 1); i++) { //LINES
+        //stars1
+        for (int j = 0; j < starLimit; j++) {
+            cout << star;
+        }
+        //spaces
+        for (int j = 0; j < spaceLimit; j++) {
+            cout << space;
+        }
+
+        //stars2
+        for (int j = 0; j < starLimit; j++) {
+            cout << star;
+        }
+
+        cout << endl;
+
+        if (i < n - 1) {
+            starLimit++;
+            spaceLimit -= 2;
+        } else {
+            starLimit--;
+            spaceLimit += 2;
+        }
+    }
+
+
+}
+
+
+int main() {
+
+
+//    makeLetterTriangle(5);
+    doubleTriangle(5);
+    return 0;
+
+}
