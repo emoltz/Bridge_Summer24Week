3a830f6752d1e330c8b513af72c2b496c6454088
emoltz <es5541@nyu.edu>
2022-08-10 15:31:04 -0400

    new shape

--- Ethan OH/Aug10.cpp
+++ Ethan OH/Aug10.cpp
@@ -3,4 +3,54 @@ using namespace std;
 
 int main(){
 
+    char inLetter;
+    cout << "Enter a lowercase lowerCase: " << endl;
+    cin >> inLetter;
+
+    char lowerCase = 'a';
+    char upperCase = 'A';
+    char space = ' ';
+    int n = (int)inLetter - (int)lowerCase + 1;
+
+    int spaceLimit = n - 1;
+    int letterLimit = 1;
+
+
+
+    for (int i = 0; i < n; i++) {
+        //SPACES
+        for (int j = 0; j < spaceLimit; j++) {
+            cout << space;
+        }
+
+        //LETTERS
+             //if the row number is even or odd, put out a different lowerCase
+        if(i % 2 == 0){ //odd row
+            //lower cases
+
+            /*
+             * Strategy one: use the number 32 to change the values
+             * Strategy two: store capital letters and swap between them
+             */
+            for (int j = 0; j < letterLimit; j++) {
+                cout << lowerCase;
+            }
+
+        }
+        else{
+            //upper cases
+            for (int j = 0; j < letterLimit; j++) {
+                cout << upperCase;
+
+            }
+        }
+
+        spaceLimit--;
+        letterLimit += 2;
+        lowerCase++;
+        upperCase++;
+
+        cout << endl;
+
+    }
 }
