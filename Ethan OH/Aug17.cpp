29d6927503d8aafbc1bf8a0e0b0b72cd44930ca8
emoltz <es5541@nyu.edu>
2022-08-17 15:18:18 -0400

    special characters

---
+++ Ethan OH/Aug17.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char tab = '\t';
+    cout << 1 << tab << 2 << tab << 3 << tab << endl;
+    cout << "\n" << "hello!" << "\n";
+
+    return 0;
+}
