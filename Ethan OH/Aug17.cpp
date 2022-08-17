4b34ff3fa1ca6823a76c17cb83da4d7995f4cbfd
emoltz <es5541@nyu.edu>
2022-08-17 15:31:01 -0400

    special characters

--- Ethan OH/Aug17.cpp
+++ Ethan OH/Aug17.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 using namespace std;
 
+int function1(bool isThisTrue){
+    //any code here
+    if (isThisTrue){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
 int main(){
     char tab = '\t';
     cout << 1 << tab << 2 << tab << 3 << tab << endl;
     cout << "\n" << "hello!" << "\n";
+
+    int a = 5;
+    int b = 6;
+
+    if (a != b){
+        cout << "They are the same";
+    }
+    else{
+        cout << "They are different";
+    }
+
+    cout << endl;
+    bool itsTrue = false;
+    cout << function1(itsTrue);
 
     return 0;
 }
