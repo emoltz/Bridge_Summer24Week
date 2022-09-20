559e9931f0e45e7508d15861384a8f6648488620
emoltz <es5541@nyu.edu>
2022-09-20 14:37:42 -0400

    exam 2 code

--- Ethan OH/Sep20.cpp
+++ Ethan OH/Sep20.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 using namespace std;
 int palindromeDistance(int* arr, int n) {
-    if (n == 0) {
-        return 0;
-    } else {
-        int first = arr[0];
-        int last = arr[n - 1];
 
-        if (first == last) {
-            return palindromeDistance(arr + 1, n - 2);
-        } else {
-            return palindromeDistance(arr + 1, n - 2) + 1;
-        }
-    }
 }
 int main(){
     int arr[6] = {1, 3, 6, 6, 5, 2};
