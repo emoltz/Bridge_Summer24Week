ec32223a8bf513c571dacb734f1531c8427b0b7e
emoltz <es5541@nyu.edu>
2022-09-20 14:56:46 -0400

    reset

--- Ethan OH/Sep20.cpp
+++ Ethan OH/Sep20.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
-int palindromeDistance(int* arr, int n) {
 
-}
+
+
 int main(){
-    int arr[6] = {1, 3, 6, 6, 5, 2};
-    cout << palindromeDistance(arr, 6);
 
     return 0;
 }
