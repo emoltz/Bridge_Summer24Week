324cf0cd49d9b700787663590a930f34d811c24b
emoltz <es5541@nyu.edu>
2022-10-04 15:17:44 -0400

    grading rules

---
+++ Ethan OH/Oct4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+/*
+ ** Grading Criteria:
+ *
+ * Does not have duplicate check numbers
+ * Interface should be clear and usable
+ * Output should be calculated correctly
+ * Most of the code is written in functions and classes
+ * Does not have an infinite loop
+ * Arrays and/or vectors don't go out of bounds
+ * Check class is well constructed
+ * Overloaded at least one operator correctly in the Check class
+ * Obvious stuff like: it compiles, it doesn't have anything illegal in it (like weird libraries) and isn't copied from the internet
+ *
+ */
+
+
+int main(){
+
+    return 0;
+}
